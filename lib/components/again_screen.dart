32a5b84868a1b60205a6ae1698f0cc2fc2efcc61
kerimkullov <libertyrx98@gmail.com>
2021-07-31 13:39:09 +0600

    add internet permission

--- lib/components/again_screen.dart
+++ lib/components/again_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get_number/components/custom_button.dart';
 
 class AgainScreen extends StatelessWidget {
   const AgainScreen({Key? key}) : super(key: key);
@@ -7,7 +8,17 @@ class AgainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Column(
-        children: [],
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(Icons.error, size: 56),
+          SizedBox(height: 16),
+          Text("OOPS, что то пошло не так"),
+          SizedBox(height: 32),
+          CustomButton(
+            onTap: () {},
+            child: Text("Попробовать снова"),
+          )
+        ],
       ),
     );
   }
